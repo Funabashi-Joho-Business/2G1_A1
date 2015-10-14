3248148e9dc9d5d0f549448f1729832d5cab0b7d
unknown <x14g023@PC504N002.chiba-fjb.ac.jp>
2015-10-14 10:54:37 +0900

    no message

--- src/Sample01.java
+++ src/Sample01.java
@@ -6,9 +6,13 @@ public static void main(String[] args) {
 		System.out.println(i);
 	}
 <<<<<<< HEAD
+	System.out.println("シオカラーズ過激派");
+=======
+<<<<<<< HEAD
 	System.out.println("加州清光折部隊");
 =======
 	System.out.println("oooeeaaeeoooooooe");
 >>>>>>> origin/sample
+>>>>>>> master
 }
 }
