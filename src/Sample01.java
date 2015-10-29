1679e9dee2d8cb53b7d45ec35d25a71ad93533c7
unknown <x14g023@PC504N002.chiba-fjb.ac.jp>
2015-10-29 09:56:16 +0900

    no message

--- src/Sample01.java
+++ src/Sample01.java
@@ -1,18 +1,6 @@
 
 public class Sample01 {
 public static void main(String[] args) {
-	System.out.println("ｆｈｄかｌ");
-	for(int i =0; i<10;i++){
-		System.out.println(i);
-	}
-<<<<<<< HEAD
-<<<<<<< HEAD
-	System.out.println("加州清光折部隊");
-=======
-	System.out.println("oooeeaaeeoooooooe");
->>>>>>> origin/sample
-=======
-	System.out.println("シオカラーズ過激派");
->>>>>>> eca0c1c4a8ce7fd9288b166bb4ecbd70d5bc2fab
+
 }
 }
